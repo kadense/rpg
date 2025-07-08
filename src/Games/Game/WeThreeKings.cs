13cf4ab6e299a7ff4bf2eb7155daf14ede1dd8d3
shaun turner <shaun.turner1@nhs.net>
2025-07-08 07:56:04 +0000

    Added LLM prompt for We Three Kings

--- src/Games/Game/WeThreeKings.cs
+++ src/Games/Game/WeThreeKings.cs
@@ -10,6 +10,11 @@ public partial class GamesFactory : GameBase
         var attr = MethodInfo.GetCurrentMethod()!.GetCustomAttribute<GameAttribute>()!;
         return this
             .WithGame(attr.Name, attr.Description ?? "TBD Add Description")
+                .WithWorldSection()
+                    .WithLlmPrompt(@"""
+                    I would like a brief narrative introduction for a game set in Judea in roman times. The troops of the nefarious King Herod roam the land causing terror to the people. On the very east of the land are three key settings, in the first first a man wielding his sword surrounded by snakes in a cloud of incense. The second, a sorceror rouses in the remains of a building that has been destroyed by lightning, marking the site of a battle with Herod's men, those that remain are dazed but the danger has not passed for the sorcerer. The third a man is in the custody of herod's men and is about to be buried alive. Describe the locations and the setting. Do not introduce the characters, just describe the setting. Do not give a title or heading. Add random elements for flair.
+                    """)
+                .End()
                 .WithCharacterSection()
                     .WithSelection("Character")
                         .WithChoice("Balthazar, the Ghost-Speaker", "Begins play being buried alive by King Herod's soldiers")
