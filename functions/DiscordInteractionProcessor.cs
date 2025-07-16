ade6fd592bc5bbd4fb9bfa802c6ee001ce52eed1
shaun turner <shaun.turner1@nhs.net>
2025-07-16 18:59:12 +0000

    fix to logic

--- functions/DiscordInteractionProcessor.cs
+++ functions/DiscordInteractionProcessor.cs
@@ -303,7 +303,7 @@ public class DiscordInteractionProcessor
 
             if (!Commands.TryGetValue(data.CustomId, out var command))
             {
-                if (SubCommands.TryGetValue(data.CustomId, out var subCommand))
+                if (!SubCommands.TryGetValue(data.CustomId, out var subCommand))
                     return ErrorResult($"command {data.CustomId} is not a recognised command.", logger);
 
                 var subCommandInstance = Activator.CreateInstance(subCommand!.CommandType);
