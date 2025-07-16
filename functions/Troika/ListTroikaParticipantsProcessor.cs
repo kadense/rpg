3120be313106bd9cd0bc89f2e0e80a41a1964074
shaun turner <shaun.turner1@nhs.net>
2025-07-16 22:58:37 +0000

    added deck options

--- functions/Troika/ListTroikaParticipantsProcessor.cs
+++ functions/Troika/ListTroikaParticipantsProcessor.cs
@@ -9,6 +9,7 @@ namespace Kadense.RPG.Troika;
 [DiscordSlashCommand("troika-list-participants", "List the participants in the Troika game.")]
 public class ListTroikaParticipantsProcessor : IDiscordSlashCommandProcessor
 {
+    private readonly KadenseRandomizer random = new KadenseRandomizer();
     private readonly DataConnectionClient client = new DataConnectionClient();
 
     [DiscordButtonCommand("refresh_participants", "Refresh the participant list")]
@@ -244,7 +245,7 @@ public class ListTroikaParticipantsProcessor : IDiscordSlashCommandProcessor
     [DiscordButtonCommand("remove_npcs", "Remove a participant from modal submission")]
     public async Task<DiscordApiResponseContent> RemoveNPCsAsync(DiscordInteraction interaction, ILogger logger)
     {
-    
+
         string guildId = interaction.GuildId ?? interaction.Guild!.Id!;
         string channelId = interaction.ChannelId ?? interaction!.Channel!.Id!;
 
@@ -395,8 +396,8 @@ public class ListTroikaParticipantsProcessor : IDiscordSlashCommandProcessor
             Response = response
         };
     }
-    
-    
+
+
 
     [DiscordButtonCommand("add_henchmen", "Adds a player from modal submission")]
     public async Task<DiscordApiResponseContent> AddHenchmenAsync(DiscordInteraction interaction, ILogger logger)
@@ -435,6 +436,97 @@ public class ListTroikaParticipantsProcessor : IDiscordSlashCommandProcessor
         return new DiscordApiResponseContent
         {
             Response = response
+        };
+    }
+
+    [DiscordButtonCommand("new_round", "Prepares the deck from the current participants list")]
+    public async Task<DiscordApiResponseContent> NewRoundAsync(DiscordInteraction interaction, ILogger logger)
+    {
+        var deck = new DeckOfCards();
+        deck.Name = "Troika Initiative";
+
+        string guildId = interaction.GuildId ?? interaction.Guild!.Id!;
+        string channelId = interaction.ChannelId ?? interaction!.Channel!.Id!;
+        // if nothing specified, then use the list of participants in the channel
+        var gameInstance = await client.ReadGameInstanceAsync(guildId, channelId);
+
+        if (gameInstance == null)
+            return TroikaResponse.GetErrorResponse("No game instance found for this channel. Please create a game first.", logger);
+
+        if (gameInstance.GameName != "Troika")
+            return TroikaResponse.GetErrorResponse("This game instance is not a Troika game. Please create a Troika game first.", logger);
+
+        foreach (var participant in gameInstance.Participants)
+        {
+            switch (participant.Type?.ToLower())
+            {
+                case "player":
+                    for (int i = 0; i < 2; i++)
+                    {
+                        deck.AddCard($"Player: {participant.Name ?? "Unknown"}");
+                    }
+                    break;
+                case "enemy":
+                    var points = participant.Attributes.GetValueOrDefault("Initiative", 1);
+                    for (int i = 0; i < points * 2; i++)
+                    {
+                        deck.AddCard($"Enemy: {participant.Name ?? "Unknown"}");
+                    }
+                    break;
+                case "henchmen":
+                    deck.AddCard($"Henchmen: {participant.Name ?? "Unknown"}");
+                    break;
+                case "ally":
+                    deck.AddCard($"Ally: {participant.Name ?? "Unknown"}");
+                    break;
+                default:
+                    deck.AddCard(participant.Name ?? "Unknown");
+                    break;
+            }
+        }
+
+        deck.AddCard($"End of Round");
+
+        if (deck.Cards.Count > 1)
+        {
+            do
+            {
+                deck.Shuffle(random);
+            } while (deck.Cards.First().Name == "End of Round");
+        }
+        await client.WriteDeckAsync(guildId, channelId, deck);
+
+
+
+        return new DiscordApiResponseContent
+        {
+            Response = TroikaResponse.NewDeckResponse(guildId, channelId, gameInstance, deck, logger)
+        };
+    }
+
+    [DiscordButtonCommand("draw_initiative", "Draw from the initiative deck")]
+    public async Task<DiscordApiResponseContent> DrawInitiativeAsync(DiscordInteraction interaction, ILogger logger)
+    {
+        string guildId = interaction.GuildId ?? interaction.Guild!.Id!;
+        string channelId = interaction.ChannelId ?? interaction!.Channel!.Id!;
+        //string userName = interaction.User!.Username ?? interaction.User!.GlobalName!.ToString();
+
+        var deck = await client.ReadDeckAsync(guildId, channelId);
+        if (deck == null)
+        {
+            throw new InvalidOperationException("Deck not found. Please create a deck first.");
+        }
+
+
+        var cardsDrawn = deck!.DrawCards(1);
+        
+        var gameInstance = await client.ReadGameInstanceAsync(guildId, channelId);
+
+        await client.WriteDeckAsync(guildId, channelId, deck);
+
+        return new DiscordApiResponseContent
+        {
+            Response = TroikaResponse.DrawInitiativeResponse(guildId, channelId, gameInstance!, deck, cardsDrawn.First(), logger)
         };
     }
 }
--- functions/Troika/TroikaResponses.cs
+++ functions/Troika/TroikaResponses.cs
@@ -127,6 +127,117 @@ public static class TroikaResponse
         return response;
     }
 
+
+    public static DiscordInteractionResponse NewDeckResponse(string guildId, string channelId, GameInstance gameInstance, DeckOfCards deck, ILogger logger, bool useOriginalMessage = true)
+    {
+        if (gameInstance.GameName != "Troika")
+        {
+            return GetErrorResponse("This game instance is not a Troika game. Please create a Troika game first.", logger, true).Response!;
+        }
+
+
+        var response = new DiscordInteractionResponseBuilder()
+            .WithResponseType(useOriginalMessage ? DiscordInteractionResponseType.UPDATE_MESSAGE : DiscordInteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE)
+            .WithData()
+                .WithFlags(1 << 15)
+                .WithContainerComponent()
+                    .WithAccentColor(0x00FF00)
+                    .WithTextDisplayComponent()
+                        .WithContent("## Troika Deck")
+                    .End()
+                    .WithTextDisplayComponent()
+                        .WithContent($"A new deck with {deck.Count()} cards has been created and shuffled.")
+                    .End()
+                    .WithActionRowComponent()
+                        .WithButtonComponent()
+                            .WithCustomId("troika-list-participants:refresh_participants")
+                            .WithLabel("Configure Deck")
+                            .WithEmoji(
+                                new DiscordEmoji()
+                                {
+                                    Name = "💪"
+                                }
+                            )
+                        .End()
+                        .WithButtonComponent()
+                            .WithCustomId("troika-list-participants:draw_initiative")
+                            .WithLabel("Draw")
+                            .WithStyle(2)
+                            .WithEmoji(
+                                new DiscordEmoji()
+                                {
+                                    Name = "🎴"
+                                }
+                            )
+                        .End()
+                    .End()
+                .End()
+            .End()
+            .Build();
+
+        logger.LogInformation($"Response: {response}");
+        return response;
+    }
+
+    
+
+    public static DiscordInteractionResponse DrawInitiativeResponse(string guildId, string channelId, GameInstance gameInstance, DeckOfCards deck, GameCard drawnCard, ILogger logger, bool useOriginalMessage = true)
+    {
+        if (gameInstance.GameName != "Troika")
+        {
+            return GetErrorResponse("This game instance is not a Troika game. Please create a Troika game first.", logger, true).Response!;
+        }
+
+
+        var builder = new DiscordInteractionResponseBuilder()
+            .WithResponseType(useOriginalMessage ? DiscordInteractionResponseType.UPDATE_MESSAGE : DiscordInteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE)
+            .WithData()
+                .WithFlags(1 << 15)
+                .WithContainerComponent()
+                    .WithAccentColor(0x00FF00)
+                    .WithTextDisplayComponent()
+                        .WithContent($"## You've drawn {drawnCard.Name}")
+                    .End()
+                    .WithTextDisplayComponent()
+                        .WithContent(drawnCard.Name == "End of Round" ? $"The round has ended" : $"There are {deck.Count()} remaining in the deck")
+                    .End()
+                    .WithActionRowComponent()
+                        .WithButtonComponent()
+                            .WithCustomId("troika-list-participants:refresh_participants")
+                            .WithLabel("End Round")
+                            .WithEmoji(
+                                new DiscordEmoji()
+                                {
+                                    Name = "🏁"
+                                }
+                            )
+                        .End();
+        if (drawnCard.Name == "")
+        {
+            builder
+                        .WithButtonComponent()
+                            .WithCustomId("troika-list-participants:draw_initiative")
+                            .WithLabel("Draw Again")
+                            .WithStyle(2)
+                            .WithEmoji(
+                                new DiscordEmoji()
+                                {
+                                    Name = "🎴"
+                                }
+                            )
+                        .End();
+        }
+        var response = builder
+                    .End()
+                .End()
+            .End()
+            .Build();
+
+        logger.LogInformation($"Response: {response}");
+        return response;
+    }
+
+
     public static DiscordInteractionResponse ListParticipantResponse(string guildId, string channelId, GameInstance gameInstance, ILogger logger, bool useOriginalMessage = true)
     {
         if (gameInstance.GameName != "Troika")
@@ -219,30 +330,30 @@ public static class TroikaResponse
                             )
                         .End()
                         .WithButtonComponent()
-                            .WithCustomId("troika-list-participants:refresh_participants")
-                            .WithLabel("Refresh")
+                            .WithCustomId("troika-list-participants:new_round")
+                            .WithLabel("New Round")
                             .WithEmoji(
                                 new DiscordEmoji()
                                 {
-                                    Name = "🔃"
+                                    Name = "🎲"
                                 }
                             )
                         .End()
                     .End()
                 .End()
-                /* 
-                .WithContainerComponent()
-                    .WithTextDisplayComponent()
-                        .WithContent("Add Player from Discord:")
+            /* 
+            .WithContainerComponent()
+                .WithTextDisplayComponent()
+                    .WithContent("Add Player from Discord:")
+                .End()
+                .WithActionRowComponent()
+                    .WithUserSelectComponent()
+                        .WithCustomId("troika_add_participants_via_select")
+                        .WithPlaceholder("Players")
                     .End()
-                    .WithActionRowComponent()
-                        .WithUserSelectComponent()
-                            .WithCustomId("troika_add_participants_via_select")
-                            .WithPlaceholder("Players")
-                        .End()
-                    .End()
-                .End() 
-                */
+                .End()
+            .End() 
+            */
             .End()
             .Build();
 
